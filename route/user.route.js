ae8bfee0740f83a1ce551966a0e4f520f5f5ece9
rajneesh <mr.rajneeshy@gmail.com>
2023-02-25 20:18:01 +0530

    done

--- route/user.route.js
+++ route/user.route.js
@@ -1,61 +1,56 @@
-const {Router} = require('express');
-const userRoute= Router()
-const {model}=require('../model/user.model')
-const jwt = require('jsonwebtoken');
-const bcrypt=require('bcrypt')
-require('dotenv').config()
-const secret =process.env.JWT_SECRET
+const { Router } = require("express");
+const userRoute = Router();
+const { model } = require("../model/user.model");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const env = require("dotenv").config();
+const secret = process.env.SECRET;
 
-userRoute.post('/register',async (req,res)=>{
-    let {name,email,gender,password,age,city}=req.body;
+userRoute.post("/register", async (req, res) => {
+  let { name, email, gender, password, age, city } = req.body;
 
-try {
-    bcrypt.hash(password, 5, async(err, hash) =>{
-        if(err){
-            res.send(err)
-        } 
-        let payload={name,email,gender,password:hash,age,city}
-        let user = model(payload);
-        await user.save();
-       res.send({"message":"user registered successfully"})
+  try {
+    bcrypt.hash(password, 5, async (err, hash) => {
+      if (err) {
+        res.send(err);
+      }
+      let payload = { name, email, gender, password: hash, age, city };
+      let user = model(payload);
+      await user.save();
+      res.send({ message: "user registered successfully" });
     });
-} catch (error) {
-    res.send(error.message)
-}
-})
+  } catch (error) {
+    res.send(error.message);
+  }
+});
 
+userRoute.post("/login", async (req, res) => {
+  // let {email, password}=req.body;
+  let email = req.body.email;
+  let password = req.body.password;
+  // let payload={
+  //     email:req.body.email,
+  //     password:req.body.password
+  // }
+  // console.log(email,password);
+  let user = await model.find({ email });
+  if (user.length == 0) {
+    res.status(404).json({ message: "user not found" });
+  }
+  try {
+    bcrypt.compare(password, user[0].password, (err, result) => {
+      if (err) {
+        res.send({ message: "login failed", err: err });
+      } else if (result) {
+        // console.log(process.env.SECRET);
 
+        let token = jwt.sign({ _id: user[0]._id }, process.env.SECRET);
+        res.send({ message: "login success", token: token });
+      }
+    });
+  } catch (error) {
+    res.send(error.message);
+  }
+});
 
-userRoute.post('/login',async (req,res)=>{
-    // let {email, password}=req.body;
-    let email = req.body.email;
-let password = req.body.password;
-// let payload={
-//     email:req.body.email,
-//     password:req.body.password
-// }
-console.log(email,password);
-    let user=await model.find({email})
-    if(user.length==0){
-        res.status(404).json({message:"user not found"})
-
-    }
-    try {
-      bcrypt.compare(password, user[0].password, (err, result)=> {
-            if(err){
-                res.send({"message":"login failed","err":err}) 
-                }else if(result){
-                    
-                    let token=jwt.sign({_id:user[0]._id} , secret)
-                    res.send({"message":"login success" ,"token":token})
-                }
-    
-        });  
-    } catch (error) {
-       res.send(error.message)
-    }
-    
-})
-
-
-module.exports={userRoute};
+module.exports = { userRoute };
